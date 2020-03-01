d5d12c5ff774460d83c931284dbc9852a933f84c
Mouhamed Fadel DIAGANA <mouhamedfd@gmail.com>
2020-03-01 18:54:25 +0000

    modifié :         tests/Feature/ExampleTest.php 	nouveau fichier : tests/Feature/RouteListTest.php

---
+++ tests/Feature/RouteListTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
+
+class RouteListTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        // $response = $this->get('/');
+
+        // $response->assertStatus(200);
+        $exitCode=Artisan::call("route:list");
+        $this->assertEquals($exitCode,0,"Probleme chargement des routes");
+
+    }
+}
